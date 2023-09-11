48717ca773b02125eb4d2646814827fbb0152196
memokey <skytopground@gmail.com>
2023-09-11 06:32:51 -0700

    changed

--- src/pages/Properties/properties.jsx
+++ src/pages/Properties/properties.jsx
@@ -16,6 +16,7 @@ export const Properties = () => {
       try {
         const response = await fetch('/v1/rentals', {
           headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/5373',
             'x-buildium-client-id': 'fedd8bc2-fe42-4865-8891-b0dfd6532dba',
             'x-buildium-client-secret': '03RYUJ+MPGyjuKdOCscRviUauD6ZUWm8R8rcuItL0yI=',
           },
